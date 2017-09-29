dedb74ea093302c86bebdda2467ee23cba5f7ab9
Albert Kaaman <albert@kaaman.nu>
2017-09-29 21:32:41 +0200

    Should hopefully resolve some issues

--- template/build/webpack.base.conf.js
+++ template/build/webpack.base.conf.js
@@ -81,12 +81,6 @@ module.exports = {
           name: utils.assetsPath('fonts/[name].[hash:7].[ext]')
         }
       }
-    ],
-    loaders: [
-      {
-        test: /\.styl$/,
-        loader: ['css-loader', 'stylus-loader'],
-      }
     ]
   }
 }
