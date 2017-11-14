e7c6b1ed3c76250a0a0f7f5bb37e89be6cb3db08
mauro <maurop123@gmail.com>
2017-11-14 17:37:13 -0500

    fix image path

--- template/src/App.vue
+++ template/src/App.vue
@@ -55,7 +55,7 @@
       <v-container fluid>
         <v-slide-y-transition mode="out-in">
           <v-layout column align-center>
-            <img src="/public/v.png" alt="Vuetify.js" class="mb-5" />
+            <img src="/static/v.png" alt="Vuetify.js" class="mb-5" />
             <blockquote>
               &#8220;First, solve the problem. Then, write the code.&#8221;
               <footer>
